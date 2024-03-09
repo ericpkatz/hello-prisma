2a25d3060685fd51c1ae348fcb5e11998d52c229
ericpkatz <ericpkatz@gmail.com>
2024-03-09 20:39:37 +0000

    add date

--- prisma/migrations/20240309203838_init/migration.sql
+++ prisma/migrations/20240309203838_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "Note" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(100) NOT NULL,
     "ranking" INTEGER NOT NULL,
+    "date_create" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
 );
